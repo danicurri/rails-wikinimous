b111838c3d6a49362a0dd779bcf6349944a84325
Daniel Contreras <danicurri7603@gmail.com>
2017-11-28 17:04:26 +0100

    starting exercise Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+puts 4 + 4
+puts Faker::Simpsons.character
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
